89d573e8b0618b20f38c2734fc5cbdb46bf1d0b9
Jason Williams <jason.williams@capaciti.org.za>
2025-12-18 10:31:00 +0200

    Initial commit

---
+++ src/Functions.java
@@ -0,0 +1,56 @@
+import java.awt.Toolkit;
+import java.util.Scanner;
+
+public class Functions {
+
+    //Change console text to red
+    public static final String RED = "\u001B[31m";
+
+    //Reset console color
+    public static final String RESET = "\u001B[0m";
+
+    // Check if inputted string was left blank
+    public static String checkEmptyString(Scanner scanner, String prompt) {
+        String input = "";  //Declaring outside loop so "input" can be available to return 
+        while (true) {
+            System.out.print(prompt);
+            input = scanner.nextLine();
+
+            //Check if "input" is empty
+            if (input.isBlank()) {
+                Toolkit.getDefaultToolkit().beep();
+                System.out.println(RED + "Field cannot be empty!" + RESET);
+            } else {
+                break;
+            }
+        }
+        //Return a value, which would be input so it can be saved in a String variable
+        return input;
+    }
+
+    //Check if inputted int was left blank
+    public static int checkEmptyInt(Scanner scanner, String prompt) {
+        int input_int = 0;
+        String input_str = "";
+        while (true) {
+            System.out.print(prompt);
+            input_str = scanner.nextLine();
+
+            //Check if "input_str" was left blank
+            if (input_str.isBlank()) {
+                Toolkit.getDefaultToolkit().beep();
+                System.out.println(RED + "Do not leave field empty!" + RESET);
+            } else {
+                //Now relevant integer checks must be done
+                try {
+                    input_int = Integer.parseInt(input_str);
+                    break;
+                } catch (NumberFormatException e) {
+                    Toolkit.getDefaultToolkit().beep();
+                    System.out.println(RED + "Enter a valid number!" + "\n"  + e + RESET);
+                }
+            } 
+        }
+        return input_int;
+    }
+}
